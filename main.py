face3655c2926590f0c07a2506c1c6bcd6f32bc0
Pavel <78315934+pavelhess73@users.noreply.github.com>
2021-12-06 08:02:44 +0000

    pandas

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import pandas
+
+data = {
+  'series_1':[1, 2, 3, 4, 5, 7],
+  'series_2':[10, 30, 40, 100, 250]
+}
+
+df = pandas.DataFrame(data)
 
 st.title('Our First Streamlit App')
 st.subheader('Interduce Streamlit Python app')
 st.write('''This is our first websiteApp.
 Enjoy it More
 ''')
+st.write(df)
